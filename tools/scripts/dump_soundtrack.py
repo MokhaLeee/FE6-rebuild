2903f1337ea153a06e699046c9664984c3dd29ac
MokhaLeee <nkulyc@163.com>
2026-05-19 15:36:27 +0000

    Add voices in process

---
+++ tools/scripts/dump_soundtrack.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+import sys, ctypes
+
+def main(args):
+    rom = "test.gba"
+    with open(rom, 'rb') as f:
+        rom_data = f.read()
+        
+        while True:
+			
+
+if __name__ == '__main__':
+    main(sys.argv)
