264546d7f749ac3f9b8b010087027ab0f057cf70
MokhaLeee <nkulyc@163.com>
2026-05-21 16:17:25 +0800

    performance optimization on skilltester

--- src/Core/SkillSys/SkillTester.iwram.c
+++ src/Core/SkillSys/SkillTester.iwram.c
@@ -7,17 +7,23 @@ bool JudgeSkillFast(struct Unit *unit, int sid)
 {
 	int pid, jid;
 
-	if (sid == 0)
+	if (unlikely(sid == 0))
 		return false;
-	if (unit == NULL || unit->pinfo == NULL)
+	if (unlikely(unit == NULL))
+		return false;
+	if (unlikely(unit->pinfo == NULL))
 		return false;
 
 	pid = UNIT_PID(unit);
-	if (gpSkillPTable[pid].sid[0] == sid || gpSkillPTable[pid].sid[1] == sid)
+	if (unlikely(gpSkillPTable[pid].sid[0] == sid))
+		return true;
+	if (unlikely(gpSkillPTable[pid].sid[1] == sid))
 		return true;
 
 	jid = UNIT_JID(unit);
-	if (gpSkillJTable[jid].sid[0] == sid || gpSkillJTable[jid].sid[1] == sid)
+	if (unlikely(gpSkillJTable[jid].sid[0] == sid))
+		return true;
+	if (unlikely(gpSkillJTable[jid].sid[1] == sid))
 		return true;
 
 	return false;
