ae1f0ebfeac5dcfc25bd7d658edf89b114f9c243
MokhaLeee <nkulyc@163.com>
2026-06-03 17:50:01 +0000

    statscreen in process

--- src/Core/NewStatscreen/newstatscreen.c
+++ src/Core/NewStatscreen/newstatscreen.c
@@ -16,6 +16,8 @@
 #include "face.h"
 #include "text.h"
 #include "msg.h"
+#include "sprite.h"
+#include "oam.h"
 #include "constants/songs.h"
 #include "constants/videoalloc_global.h"
 
@@ -23,32 +25,41 @@
 
 extern const u16 Tsa_Mss_Upper[];
 extern const u16 Tsa_Mss_Left[];
-extern const u16 Tsa_Mss_Right[];
 extern const u8 Img_MssUI[];
 extern const u16 Pal_MssUI[0x10];
+extern const u8 Img_MssUI2[];
+extern const u16 Pal_MssUI2[0x10];
+extern const u8 Img_MssSprites[];
 
-static EWRAM_OVERLAY(0) u16 TmBuff_MssR2[0x1C0] = {}; // 12 * 14
-static EWRAM_OVERLAY(0) u16 TmBuff_MssR1[0x1C0] = {}; // 12 * 14
-static EWRAM_OVERLAY(0) u16 TmBuff_MssR0[0x1C0] = {}; // 12 * 14
-static EWRAM_OVERLAY(0) u16 TmBuff_MssL2[0x1C0] = {}; // 18 * 14
-static EWRAM_OVERLAY(0) u16 TmBuff_MssL1[0x1C0] = {}; // 18 * 14
-static EWRAM_OVERLAY(0) u16 TmBuff_MssL0[0x1C0] = {}; // 18 * 14
-static EWRAM_OVERLAY(0) u16 TmBuff_MssU1[0xC0] = {}; // 24 * 6
 static EWRAM_OVERLAY(0) u16 TmBuff_MssU0[0xC0] = {}; // 24 * 6
+static EWRAM_OVERLAY(0) u16 TmBuff_MssU1[0xC0] = {}; // 24 * 6
+static EWRAM_OVERLAY(0) u16 TmBuff_MssL0[0x1C0] = {}; // 18 * 14
+static EWRAM_OVERLAY(0) u16 TmBuff_MssL1[0x1C0] = {}; // 18 * 14
+static EWRAM_OVERLAY(0) u16 TmBuff_MssL2[0x1C0] = {}; // 18 * 14
+static EWRAM_OVERLAY(0) u16 TmBuff_MssR0[0x200] = {}; // 16 * 14
+static EWRAM_OVERLAY(0) u16 TmBuff_MssR2[0x200] = {}; // 16 * 14
+
+static const struct ProcScr proc_mss_sprites[];
 
 enum videoalloc_mss {
 	BGCHR_MSS_TEXT = 0,
-	BGCHR_MSS_FACE = 0x2400 / 0x20,
-	BGCHR_MSS_UI = 0x2800 / 0x20,
-	BGCHR_MSS_STATBAR = 0x3000 / 0x20,
+	BGCHR_MSS_FACE = 0x3000 / 0x20,
+	BGCHR_MSS_UI = 0x3400 / 0x20,
+	BGCHR_MSS_UI2 = 0x3C00 / 0x20,
+	BGCHR_MSS_STATBAR = 0x4400 / 0x20,
 
 	BGPAL_MSS_TEXT = 0,
-	BGPAL_MSS_UI = 1,
-	BGPAL_MSS_STATBAR = 2,
+	BGPAL_MSS_UI = 1, // .. 2
 	BGPAL_MSS_FACE = 3,
 	BGPAL_MSS_ICON0 = 4,
 	BGPAL_MSS_ICON1 = 5,
 	BGPAL_MSS_TILESET = 6, // .. 15
+
+	BGPAL_MSS_STATBAR = BGPAL_MSS_UI + 1,
+
+
+	OBCHR_MSS_SPRITES = 0x7000 / 0x20,
+	OBPAL_MSS_SPRITES = 2,
 };
 
 enum mss_text_index {
@@ -148,9 +159,10 @@ static void mss_init(ProcPtr proc)
 	InitTextList(mss_text_init_info);
 
 	ApplyIconPalettes(BGPAL_MSS_ICON0);
-	ApplyUiStatBarPal(BGPAL_MSS_STATBAR);
 	ApplyPalette(Pal_MssUI, BGPAL_MSS_UI);
+	ApplyPalette(Pal_MssUI2, BGPAL_MSS_STATBAR);
 	Decompress(Img_MssUI, (u8 *)BG_VRAM + BGCHR_MSS_UI * CHR_SIZE);
+	Decompress(Img_MssUI2, (u8 *)BG_VRAM + BGCHR_MSS_UI2 * CHR_SIZE);
 
 	BattleGenerateDisplayStats(unit, GetUnitEquippedWeaponSlot(unit));
 
@@ -162,8 +174,9 @@ static void mss_init(ProcPtr proc)
 	CpuFastFill(0, TmBuff_MssL2, sizeof(TmBuff_MssL2));
 
 	CpuFastFill(0, TmBuff_MssR0, sizeof(TmBuff_MssR0));
-	CpuFastFill(0, TmBuff_MssR1, sizeof(TmBuff_MssR1));
 	CpuFastFill(0, TmBuff_MssR2, sizeof(TmBuff_MssR2));
+
+	SpawnProc(proc_mss_sprites, proc);
 }
 
 static void mss_put_page_upper(ProcPtr proc)
@@ -180,7 +193,7 @@ static void mss_put_page_upper(ProcPtr proc)
 		BGCHR_MSS_FACE, BGPAL_MSS_FACE, FALSE);
 
 	/* level & exp */
-	x_offset = 6;
+	x_offset = 5;
 
 	PutTwoSpecialChar(TmBuff_MssU0 + TM_OFFSET(x_offset + 0, 1), TEXT_COLOR_SYSTEM_GOLD, TEXT_SPECIAL_LV_A, TEXT_SPECIAL_LV_B);
 	PutNumberOrBlank(TmBuff_MssU0 + TM_OFFSET(x_offset + 3, 1), TEXT_COLOR_SYSTEM_BLUE, unit->level);
@@ -194,7 +207,7 @@ static void mss_put_page_upper(ProcPtr proc)
 	PutNumberOrBlank(TmBuff_MssU0 + TM_OFFSET(x_offset + 6, 3), TEXT_COLOR_SYSTEM_BLUE, GetUnitMaxHp(unit));
 
 	/* p/j name */
-	x_offset = 15;
+	x_offset = 12;
 
 	str = DecodeMsg(unit->pinfo->msg_name);
 	x_text = GetStringTextCenteredPos(8 * 7, str);
@@ -213,29 +226,15 @@ static void mss_put_page_upper(ProcPtr proc)
 		TmBuff_MssU0 + TM_OFFSET(x_offset, 3),
 		TEXT_COLOR_SYSTEM_WHITE,
 		x_text, 0, str);
-
-	/* affin */
-	x_offset = 24;
-
-	if (unit->pinfo->affinity)
-		PutIcon(TmBuff_MssU0 + TM_OFFSET(x_offset, 1),
-			GetUnitAffinityIcon(unit), TILEREF(0, BGPAL_ICONS + 1));
 }
 
-static struct StatScreenTextInfo const mss_textinfo_page1_left[] = {
-	{ mss_st.texts + MSS_TEXT_POW, TmBuff_MssL0 + TM_OFFSET(1, 1),  TEXT_COLOR_SYSTEM_GOLD, 0, "Str" },
-	{ mss_st.texts + MSS_TEXT_MAG, TmBuff_MssL0 + TM_OFFSET(1, 3),  TEXT_COLOR_SYSTEM_GOLD, 0, "Mag" },
-	{ mss_st.texts + MSS_TEXT_SKL, TmBuff_MssL0 + TM_OFFSET(1, 5),  TEXT_COLOR_SYSTEM_GOLD, 0, "Skl" },
-	{ mss_st.texts + MSS_TEXT_SPD, TmBuff_MssL0 + TM_OFFSET(1, 7),  TEXT_COLOR_SYSTEM_GOLD, 0, "Spd" },
-	{ mss_st.texts + MSS_TEXT_DEF, TmBuff_MssL0 + TM_OFFSET(1, 9),  TEXT_COLOR_SYSTEM_GOLD, 0, "Def" },
-	{ mss_st.texts + MSS_TEXT_RES, TmBuff_MssL0 + TM_OFFSET(1, 11), TEXT_COLOR_SYSTEM_GOLD, 0, "Res" },
+static void mss_put_page_left(ProcPtr proc)
+{
+	Decompress(Tsa_Mss_Left, gBuf);
+	TmApplyTsa(TmBuff_MssL1, gBuf, TILEREF(BGCHR_MSS_UI, BGPAL_MSS_UI));
+}
 
-	{ mss_st.texts + MSS_TEXT_LCK, TmBuff_MssL0 + TM_OFFSET(9, 1),  TEXT_COLOR_SYSTEM_GOLD, 0, "Luck" },
-	{ mss_st.texts + MSS_TEXT_MOV, TmBuff_MssL0 + TM_OFFSET(9, 3),  TEXT_COLOR_SYSTEM_GOLD, 0, "Move" },
-	{ 0 }, // end
-};
-
-static void mss_put_stat(int num, int x, int y, int base, int total, int max)
+static void mss_put_stat(int num, int x, int y, int base, int total, int max, u16 *tm)
 {
 	int bonus = total - base;
 
@@ -246,35 +245,39 @@ static void mss_put_stat(int num, int x, int y, int base, int total, int max)
 
 	PutDrawUiGauge(
 		BGCHR_MSS_STATBAR + 1 + num * 6, 6,
-		TmBuff_MssL2 + TM_OFFSET(x - 2, y + 1),
+		tm + TM_OFFSET(x - 2, y + 1),
 		TILEREF(0, BGPAL_MSS_STATBAR),
 		k_udiv(max * 41, 30),
 		k_udiv(base * 41, 30),
 		bonus > 0 ? k_udiv(bonus * 41, 30) : -k_udiv(-bonus * 41, 30));
 }
 
-static void mss_put_page_left(ProcPtr proc)
-{
-	struct Unit *unit = mss_st.unit;
+static struct StatScreenTextInfo const mss_textinfo_page1_right[] = {
+	{ mss_st.texts + MSS_TEXT_POW, TmBuff_MssR0 + TM_OFFSET(1, 0),  TEXT_COLOR_SYSTEM_GOLD, 0, "Str" },
+	{ mss_st.texts + MSS_TEXT_MAG, TmBuff_MssR0 + TM_OFFSET(1, 2),  TEXT_COLOR_SYSTEM_GOLD, 0, "Mag" },
+	{ mss_st.texts + MSS_TEXT_SKL, TmBuff_MssR0 + TM_OFFSET(1, 4),  TEXT_COLOR_SYSTEM_GOLD, 0, "Skl" },
+	{ mss_st.texts + MSS_TEXT_SPD, TmBuff_MssR0 + TM_OFFSET(1, 6),  TEXT_COLOR_SYSTEM_GOLD, 0, "Spd" },
+	{ mss_st.texts + MSS_TEXT_DEF, TmBuff_MssR0 + TM_OFFSET(1, 8),  TEXT_COLOR_SYSTEM_GOLD, 0, "Def" },
+	{ mss_st.texts + MSS_TEXT_RES, TmBuff_MssR0 + TM_OFFSET(1, 10), TEXT_COLOR_SYSTEM_GOLD, 0, "Res" },
 
-	Decompress(Tsa_Mss_Left, gBuf);
-	TmApplyTsa(TmBuff_MssL1, gBuf, TILEREF(BGCHR_MSS_UI, BGPAL_MSS_UI));
-
-	PutStatScreenText(mss_textinfo_page1_left);
-
-	mss_put_stat(0, 5, 1, unit->pow, GetUnitPower(unit), GetUnitMaxStatusPow(unit));
-	mss_put_stat(1, 5, 3, unit->mag, GetUnitMagic(unit), GetUnitMaxStatusMag(unit));
-	mss_put_stat(2, 5, 5, unit->skl, GetUnitSkill(unit), GetUnitMaxStatusSkl(unit));
-	mss_put_stat(3, 5, 7, unit->spd, GetUnitSpeed(unit), GetUnitMaxStatusSpd(unit));
-	mss_put_stat(4, 5, 9, unit->def, GetUnitDefense(unit), GetUnitMaxStatusDef(unit));
-	mss_put_stat(5, 5, 11, unit->res, GetUnitResistance(unit), GetUnitMaxStatusRes(unit));
-	mss_put_stat(6, 13, 1, unit->lck, GetUnitLuck(unit), GetUnitMaxStatusLck(unit));
-}
+	{ mss_st.texts + MSS_TEXT_LCK, TmBuff_MssR0 + TM_OFFSET(1, 12),  TEXT_COLOR_SYSTEM_GOLD, 0, "Lck" },
+	{ mss_st.texts + MSS_TEXT_MOV, TmBuff_MssR0 + TM_OFFSET(1, 14),  TEXT_COLOR_SYSTEM_GOLD, 0, "Mov" },
+	{ 0 }, // end
+};
 
 static void mss_put_page_right(ProcPtr proc)
 {
-	Decompress(Tsa_Mss_Right, gBuf);
-	TmApplyTsa(TmBuff_MssR1, gBuf, TILEREF(BGCHR_MSS_UI, BGPAL_MSS_UI));
+	struct Unit *unit = mss_st.unit;
+
+	PutStatScreenText(mss_textinfo_page1_right);
+
+	mss_put_stat(0, 5, 0,  unit->pow, GetUnitPower(unit), GetUnitMaxStatusPow(unit), TmBuff_MssR2);
+	mss_put_stat(1, 5, 2,  unit->mag, GetUnitMagic(unit), GetUnitMaxStatusMag(unit), TmBuff_MssR2);
+	mss_put_stat(2, 5, 4,  unit->skl, GetUnitSkill(unit), GetUnitMaxStatusSkl(unit), TmBuff_MssR2);
+	mss_put_stat(3, 5, 6,  unit->spd, GetUnitSpeed(unit), GetUnitMaxStatusSpd(unit), TmBuff_MssR2);
+	mss_put_stat(4, 5, 8,  unit->def, GetUnitDefense(unit), GetUnitMaxStatusDef(unit), TmBuff_MssR2);
+	mss_put_stat(5, 5, 10, unit->res, GetUnitResistance(unit), GetUnitMaxStatusRes(unit), TmBuff_MssR2);
+	mss_put_stat(6, 5, 12, unit->lck, GetUnitLuck(unit), GetUnitMaxStatusLck(unit), TmBuff_MssR2);
 }
 
 static void mss_prepare_display(ProcPtr proc)
@@ -286,12 +289,11 @@ static void mss_prepare_display(ProcPtr proc)
 
 	TmCopyRect(TmBuff_MssU0, gBg0Tm + TM_OFFSET(0, 0), 30, 6);
 	TmCopyRect(TmBuff_MssU1, gBg1Tm + TM_OFFSET(0, 0), 30, 6);
-	TmCopyRect(TmBuff_MssL0, gBg0Tm + TM_OFFSET(0, 6), 18, 14);
-	TmCopyRect(TmBuff_MssL1, gBg1Tm + TM_OFFSET(0, 6), 18, 14);
-	TmCopyRect(TmBuff_MssL2, gBg2Tm + TM_OFFSET(0, 6), 18, 14);
-	TmCopyRect(TmBuff_MssR0, gBg0Tm + TM_OFFSET(18, 6), 12, 14);
-	TmCopyRect(TmBuff_MssR1, gBg1Tm + TM_OFFSET(18, 6), 12, 14);
-	TmCopyRect(TmBuff_MssR2, gBg2Tm + TM_OFFSET(18, 6), 12, 14);
+	TmCopyRect(TmBuff_MssL0, gBg0Tm + TM_OFFSET(0, 6), 20, 14);
+	TmCopyRect(TmBuff_MssL1, gBg1Tm + TM_OFFSET(0, 6), 20, 14);
+	TmCopyRect(TmBuff_MssL2, gBg2Tm + TM_OFFSET(0, 6), 20, 14);
+	TmCopyRect(TmBuff_MssR0, gBg0Tm + TM_OFFSET(20, 2), 12, 16);
+	TmCopyRect(TmBuff_MssR2, gBg2Tm + TM_OFFSET(20, 2), 12, 16);
 
 	EnableBgSync(BG0_SYNC_BIT | BG1_SYNC_BIT | BG2_SYNC_BIT);
 }
@@ -304,12 +306,70 @@ static void mss_end(ProcPtr proc)
 	TmFill(gBg1Tm, 0);
 	TmFill(gBg2Tm, 0);
 	EnableBgSync(BG0_SYNC_BIT | BG1_SYNC_BIT | BG2_SYNC_BIT);
+
+	Proc_End(FindProc(proc_mss_sprites));
 }
+
+/**
+ * sprites
+ */
+static void mss_sprites_init(ProcPtr proc)
+{
+	Decompress(Img_MssSprites, OBJ_VRAM0 + OBCHR_MSS_SPRITES * CHR_SIZE);
+	ApplyIconPalette(1, 0x10 + OBPAL_MSS_SPRITES);
+}
+
+static const u16 sprite_left_window[] = {
+	20,
+	OAM0_SHAPE_8x32  + OAM0_Y(0) + OAM0_BLEND, OAM1_SIZE_8x32 + OAM1_HFLIP + OAM1_X(-8), OAM2_CHR(0x4),
+	OAM0_SHAPE_8x32  + OAM0_Y(32) + OAM0_BLEND, OAM1_SIZE_8x32 + OAM1_HFLIP + OAM1_X(-8) + OAM1_VFLIP, OAM2_CHR(0x4),
+	OAM0_SHAPE_8x32  + OAM0_Y(60) + OAM0_BLEND, OAM1_SIZE_8x32 + OAM1_HFLIP + OAM1_X(-8) + OAM1_VFLIP, OAM2_CHR(0x4),
+	OAM0_SHAPE_8x32  + OAM0_Y(90) + OAM0_BLEND, OAM1_SIZE_8x32 + OAM1_HFLIP + OAM1_X(-8) + OAM1_VFLIP, OAM2_CHR(0x4),
+	OAM0_SHAPE_8x32  + OAM0_Y(108) + OAM0_BLEND, OAM1_SIZE_8x32 + OAM1_HFLIP + OAM1_X(-8) + OAM1_VFLIP, OAM2_CHR(0x4),
+
+	OAM0_SHAPE_32x32 + OAM0_Y(0) + OAM0_BLEND, OAM1_SIZE_32x32 + OAM1_X(0), 0,
+	OAM0_SHAPE_32x32 + OAM0_Y(0) + OAM0_BLEND, OAM1_SIZE_32x32 + OAM1_X(32), 0,
+
+	OAM0_SHAPE_32x32 + OAM0_Y(32) + OAM0_BLEND, OAM1_SIZE_32x32 + OAM1_X(0), 0,
+	OAM0_SHAPE_32x32 + OAM0_Y(32) + OAM0_BLEND, OAM1_SIZE_32x32 + OAM1_X(32), 0,
+
+	OAM0_SHAPE_32x32 + OAM0_Y(64) + OAM0_BLEND, OAM1_SIZE_32x32 + OAM1_X(0), 0,
+	OAM0_SHAPE_32x32 + OAM0_Y(64) + OAM0_BLEND, OAM1_SIZE_32x32 + OAM1_X(32), 0,
+
+	OAM0_SHAPE_32x32 + OAM0_Y(96) + OAM0_BLEND, OAM1_SIZE_32x32 + OAM1_X(0), 0,
+	OAM0_SHAPE_32x32 + OAM0_Y(96) + OAM0_BLEND, OAM1_SIZE_32x32 + OAM1_X(32), 0,
+
+	OAM0_SHAPE_32x32 + OAM0_Y(108) + OAM0_BLEND, OAM1_SIZE_32x32 + OAM1_X(0), 0,
+	OAM0_SHAPE_32x32 + OAM0_Y(108) + OAM0_BLEND, OAM1_SIZE_32x32 + OAM1_X(32), 0,
+
+	OAM0_SHAPE_8x32  + OAM0_Y(0) + OAM0_BLEND, OAM1_SIZE_8x32  + OAM1_X(64), OAM2_CHR(0x4),
+	OAM0_SHAPE_8x32  + OAM0_Y(32) + OAM0_BLEND, OAM1_SIZE_8x32  + OAM1_X(64) + OAM1_VFLIP, OAM2_CHR(0x4),
+	OAM0_SHAPE_8x32  + OAM0_Y(60) + OAM0_BLEND, OAM1_SIZE_8x32  + OAM1_X(64) + OAM1_VFLIP, OAM2_CHR(0x4),
+	OAM0_SHAPE_8x32  + OAM0_Y(90) + OAM0_BLEND, OAM1_SIZE_8x32  + OAM1_X(64) + OAM1_VFLIP, OAM2_CHR(0x4),
+	OAM0_SHAPE_8x32  + OAM0_Y(108) + OAM0_BLEND, OAM1_SIZE_8x32  + OAM1_X(64) + OAM1_VFLIP, OAM2_CHR(0x4),
+};
+
+static void mss_sprites_loop(ProcPtr proc)
+{
+	PutSprite(
+		12,
+		166, 12,
+		sprite_left_window,
+		OAM2_CHR(OBCHR_MSS_SPRITES) + OAM2_PAL(OBPAL_MSS_SPRITES) + OAM2_LAYER(3)
+	);
+}
+
+static const struct ProcScr proc_mss_sprites[] = {
+	PROC_CALL(mss_sprites_init),
+	PROC_YIELD,
+	PROC_REPEAT(mss_sprites_loop),
+	PROC_END
+};
 
 /**
  * main
  */
-static const struct ProcScr procscr_modern_statscreen[] = {
+static const struct ProcScr proc_modern_statscreen[] = {
 	PROC_CALL(LockBmDisplay),
 
 	/* remove map-ui */
@@ -340,5 +400,5 @@ void StartModernStatScreen(struct Unit *unit, ProcPtr parent)
 	PidStatsAddStatView(unit->pinfo->id);
 	PlaySe(SONG_6A);
 
-	SpawnProcLocking(procscr_modern_statscreen, parent);
+	SpawnProcLocking(proc_modern_statscreen, parent);
 }
--- src/Core/Statscreen/NegativeStatBoosts/statbar.c
+++ src/Core/Statscreen/NegativeStatBoosts/statbar.c
@@ -3,32 +3,60 @@
 #include "hardware.h"
 #include "util.h"
 
+enum statbar_palettes {
+#if CONFIG_USE_MODERN_STATSCREEN
+	STATBAR_PAL_1 = 1,
+	STATBAR_PAL_3 = 3,
+	STATBAR_PAL_4 = 4,
+	STATBAR_PAL_5 = 5,
+	STATBAR_PAL_7 = 7,
+	STATBAR_PAL_8 = 8,
+
+	STATBAR_PAL_12 = 2,
+	STATBAR_PAL_13 = 6,
+	STATBAR_PAL_14 = 9,
+
+	/* 10-15 is free */
+#else
+	STATBAR_PAL_1 = 1,
+	STATBAR_PAL_3 = 3,
+	STATBAR_PAL_4 = 4,
+	STATBAR_PAL_5 = 5,
+	STATBAR_PAL_7 = 7,
+	STATBAR_PAL_8 = 8,
+
+	STATBAR_PAL_12 = 12,
+	STATBAR_PAL_13 = 13,
+	STATBAR_PAL_14 = 14,
+#endif
+};
+
 static void DrawUiGaugeBitmapEdgeColumn(u8 *bitmap, int pixels_per_line, int column)
 {
-	bitmap[1 * pixels_per_line + column] = 4;
-	bitmap[2 * pixels_per_line + column] = 14;
-	bitmap[3 * pixels_per_line + column] = 3;
+	bitmap[1 * pixels_per_line + column] = STATBAR_PAL_4;
+	bitmap[2 * pixels_per_line + column] = STATBAR_PAL_14;
+	bitmap[3 * pixels_per_line + column] = STATBAR_PAL_3;
 }
 
 static void DrawUiGaugeBitmapBaseColumn(u8 *bitmap, int pixels_per_line, int column)
 {
-	bitmap[0 * pixels_per_line + column] = 4;
-	bitmap[1 * pixels_per_line + column] = 14;
-	bitmap[2 * pixels_per_line + column] = 14;
-	bitmap[3 * pixels_per_line + column] = 14;
-	bitmap[4 * pixels_per_line + column] = 3;
+	bitmap[0 * pixels_per_line + column] = STATBAR_PAL_4;
+	bitmap[1 * pixels_per_line + column] = STATBAR_PAL_14;
+	bitmap[2 * pixels_per_line + column] = STATBAR_PAL_14;
+	bitmap[3 * pixels_per_line + column] = STATBAR_PAL_14;
+	bitmap[4 * pixels_per_line + column] = STATBAR_PAL_3;
 }
 
 static void DrawUiGaugeBitmapFilledColumn(u8 *bitmap, int pixels_per_line, int column)
 {
-	bitmap[1 * pixels_per_line + column] = 1;
-	bitmap[2 * pixels_per_line + column] = 5;
+	bitmap[1 * pixels_per_line + column] = STATBAR_PAL_1;
+	bitmap[2 * pixels_per_line + column] = STATBAR_PAL_5;
 }
 
 static void DrawUiGaugeBitmapBonusColumn(u8 *bitmap, int pixels_per_line, int column)
 {
-	bitmap[1 * pixels_per_line + column] = 13;
-	bitmap[2 * pixels_per_line + column] = 12;
+	bitmap[1 * pixels_per_line + column] = STATBAR_PAL_13;
+	bitmap[2 * pixels_per_line + column] = STATBAR_PAL_12;
 }
 
 static void DrawUiGauge(int chr, int dot_x, int chr_count, int dot_width, int dot_plain, int dot_bonus)
@@ -62,8 +90,8 @@ static void DrawUiGauge(int chr, int dot_x, int chr_count, int dot_width, int do
 			int pixels_per_line = chr_count * 8;
 			int column = dot_plain - i + 2 + dot_x;
 
-			bitmap[1 * pixels_per_line + column] = 8;
-			bitmap[2 * pixels_per_line + column] = 7;
+			bitmap[1 * pixels_per_line + column] = STATBAR_PAL_8;
+			bitmap[2 * pixels_per_line + column] = STATBAR_PAL_7;
 		}
 	}
 
